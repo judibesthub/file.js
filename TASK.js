400e8a8c5afcd8108b0e4b69654f3cec8a088fe3
judibesthub <61976890+judibesthub@users.noreply.github.com>
2020-03-27 18:37:23 +0100

    Add files via upload

---
+++ TASK.js
@@ -0,0 +1,20 @@
+TASK.js
+// a variable to store name
+
+let name = "Judith Ozioma";
+
+// an array to store courses
+
+let courses = ["HTML","CSS","Javascript"];
+
+//display name and courses
+
+console.log('my name is Judith Ozioma and my courses are HTML,CSS,Javascript');
+
+//show odd number of courses length within 1-200 range
+
+for(let i=0; i<200; i++){
+    if (i%2 !==0){
+        console.log(i)
+    }
+}
